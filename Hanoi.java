8c445f931918c9c72893a99045f8d0b76798c0be
raphaelexander <benalexander2020@gmail.com>
2025-02-13 14:39:49 -0800

    Hanoi program is final, hanoitest was initial iteration. Will get to it later

--- Hanoi.java
+++ Hanoi.java
@@ -2,118 +2,59 @@ import java.util.Arrays;
 
 public class Hanoi {
 
-    public static void main(String args[]) {
-        int[] tower1 = {1, 2, 3};
-        int[] tower2 = {0, 0, 0};
+    public static void main(String args[]) { 
+        int[] tower1 = {1, 2, 3}; // 1 is the smallest disk, 3 is the largest disk
+        int[] tower2 = {0, 0, 0};// 0 means no disk
         int[] tower3 = {0, 0, 0};
 
-        System.out.println("Three towers stand: " + Arrays.toString(tower1) + Arrays.toString(tower2) + Arrays.toString(tower3));
-        Hanoi.Towers(tower1, tower2, tower3);
-        System.out.println("After recursion, three towers still stand: " + Arrays.toString(tower1) + Arrays.toString(tower2) + Arrays.toString(tower3));
-    }
+        // print the initial state of the three towers
+        System.out.println("Three towers stand: " + Arrays.toString(tower1) + Arrays.toString(tower2) + Arrays.toString(tower3)); 
+        Towers(tower1.length, tower1, tower2, tower3);// move all disks from tower1 to tower3
+        System.out.println("After recursion, three towers still stand: " + Arrays.toString(tower1) + Arrays.toString(tower2) + Arrays.toString(tower3));// print the final state of the three towers
+    } // end of main
 
-    public static int[] Towers(int[] tower1, int[] tower2, int[] tower3) {
-        int top1 = 0; 
-        int top2 = 0;
-        int top3 = 0;
-        if (tower3[0] == 0) { //if target tower is empty
-            for (int i = 0; i < tower1.length; i++) { //find largest block in start tower
-                if (tower1[i] > top1) {
-                    top1 = tower1[i];
-                }
-            }
-            tower3[0] = top1; //base of target tower is largest block from start tower
-            top1 = 0;
-            return Towers(tower1, tower2, tower3); //recursion
+    public static void Towers(int n, int[] tower1, int[] tower2, int[] tower3) {
+        if (n > 0) { // move n-1 disks from tower1 to tower2 using tower3
+            Towers(n - 1, tower1, tower3, tower2);// move n-1 disks from tower1 to tower2 using tower3
+            moveDisk(tower1, tower3);// move the top disk from tower1 to tower3
+            System.out.println("Moved disk from tower1 to tower3: " + Arrays.toString(tower1) + Arrays.toString(tower2) + Arrays.toString(tower3));
+             // print the state after moving disk
+            Towers(n - 1, tower2, tower1, tower3);// move n-1 disks from tower2 to tower3 using tower1
+        }// end of if
+    }// end of Towers
 
-        } else if (top1 < tower3[0]) { //if new largest block from start tower is bigger than block on top of target tower
-            if (tower2[0] == 0) { //if aux tower is empty
-                for (int i = 0; i < tower1.length; i++) { //find largest block in start tower
-                if (tower1[i] > top1) {
-                    top1 = tower1[i];
-                }
-            }
-                tower2[0] = top1;//base of aux tower is largest block from start tower
-                top2 = 0; //top of start tower is null
-                return Towers(tower1, tower2, tower3);
+    public static void moveDisk(int[] fromTower, int[] toTower) { // move the top disk from fromTower to toTower
+        int fromIndex = -1; // find the index of the top disk in fromTower
+        int toIndex = -1; 
 
-            } else if (tower2[1] != 0){ //if aux tower has a block
-                for (int i = 0; i < tower3.length; i++ ) {
-                    if (tower3[i] > top3) {
-                        top3 = tower3[i];
-                    }
-                }
-                tower2[1] = top3; //then top block from target tower goes on top of aux tower
-                tower3[0] = 0; //target tower is empty
-                
-                return Towers(tower1, tower2, tower3);
-            }
-        } else if (tower3[0] == 1) { //if base of target has largest block
-            for (int i = 0; i < tower2.length; i++) { //find largest block in aux
-                if (tower2[i] > top2) {
-                    top2 = tower2[i];
-                }
-            }
-            if (top2 > tower1[0]) { //if start tower has no block
-                tower1[0] = top2; //base of start tower is previous top of aux tower
-                tower2[top2] = 0; //top of aux tower empty
-                Towers(tower1, tower2, tower3);
-            } else if (top2 < tower1[0]) { //if start tower does have block
-                for (int i = 0; i < tower2.length; i++) { //find largest block in aux
-                    if (tower2[i] > top2) {
-                        top2 = tower2[i];
-                }
-            }
-                tower3[1] = top2;
-                tower2[top2] = 0;
-                Towers(tower1, tower2, tower3);
-            } else if (tower3[1] == 2) {
-                for (int i = 0; i < tower1.length; i++) { //find largest block in start
-                    if (tower1[i] > top1) {
-                        top1 = tower1[i];
-                    }
-                }
-                tower3[2] = top1;
-                tower1[0] = 0;
+        for (int i = 0; i < fromTower.length; i++) { 
+            // find the index of the first non-zero element in fromTower
+            if (fromTower[i] != 0) {
+                fromIndex = i;// store the index of the top disk in fromTower
+                break;
             }
         }
-        return tower3;
-    }
-}
 
-//     public boolean checkTowers(int[] tower1, int[] tower2, int[] tower3){
-//         boolean check = true;
-//         if(tower3[2] != 3){
-//             for (int block : tower1) {
-//             }
-//             for (int block : tower2) {
-//                 if(tower2[block] > tower2[block + 1]){
-//                     check = false;
-//                 }
-//             }
-//             for (int block : tower3) {
-//                 if(tower3[block] > tower3[block + 1]){
-//                     check = false;
-//                 }
-//             }
-//         }
-//         return check;
-//     }
-// }
+        for (int i = toTower.length - 1; i >= 0; i--) {// find the index of the first zero element in toTower
+            if (toTower[i] == 0) {
+                toIndex = i;// store the index of the top disk in toTower
+                break;
+            }
+        }
 
-
-// tower1 base move to tower3
-// if block on top of base
-// find smallest block, move to target
-//if no block on top of tower1 base
-//try to move to tower3
-//if smaller block below, move that block to tower2
-//else move to tower3
-//if big block at base of tower3, if smallest block on tower2, move to tower1
-//else move remaining block from tower2 to tower 3
-// reiterate
-
-
-//smallest block is 2, try to move to target
-//move to aux tower
-//reiterate
+        if (fromIndex != -1 && toIndex != -1) { // move the top disk from fromTower to toTower
+            toTower[toIndex] = fromTower[fromIndex];
+            fromTower[fromIndex] = 0; // remove the top disk from fromTower
+       
+        } // end of if
+    } // end of moveDisk
+} // end of Hanoi.java
+//ouput of the program is: Three towers stand: [1, 2, 3][0, 0, 0][0, 0, 0]
+//Moved disk from tower1 to tower3: [0, 2, 3][0, 0, 0][0, 0, 1]
+//Moved disk from tower1 to tower3: [0, 0, 3][0, 0, 1][0, 0, 2]
+//Moved disk from tower1 to tower3: [0, 0, 0][0, 0, 3][0, 1, 2]
+//Moved disk from tower1 to tower3: [0, 0, 0][0, 1, 2][0, 0, 3]
+//Moved disk from tower1 to tower3: [0, 0, 2][0, 0, 3][0, 0, 1]
+//Moved disk from tower1 to tower3: [0, 0, 0][0, 0, 1][0, 2, 3]
+//Moved disk from tower1 to tower3: [0, 0, 0][0, 0, 0][1, 2, 3]
+//After recursion, three towers still stand: [0, 0, 0][0, 0, 0][1, 2, 3]
